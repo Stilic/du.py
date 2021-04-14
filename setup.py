5a16ce874ecbf244a7fe6a7d2b0103cbce23a8cd
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-04-14 10:38:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="du.py",
-    version="alpha-1",
+    version="1.0.0",
     author="Stylix58",
     author_email="lateman-jpeg@outlook.fr",
     description="A API wrapper for Dangerous Users Data Base",
